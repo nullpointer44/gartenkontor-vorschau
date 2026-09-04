fe930ba38bf90dc6d084857554eff29950d5bfde
Philipp Awwad <pa@awwad.eu>
2026-09-04 11:59:07 +0200

    Vorschau 2026-09-04 11:59

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://had-generator-dress-simple.trycloudflare.com/api/orders"
+  orderEndpoint: "https://autumn-insight-clearance-spears.trycloudflare.com/api/orders"
 };
