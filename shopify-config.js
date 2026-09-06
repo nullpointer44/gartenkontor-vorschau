dea4373f39a250f775fc458dc1060b082c570010
Philipp Awwad <pa@awwad.eu>
2026-09-06 10:49:43 +0200

    Vorschau 2026-09-06 10:49

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://ended-knit-stranger-transport.trycloudflare.com/api/orders"
+  orderEndpoint: "https://lovely-even-baskets-tour.trycloudflare.com/api/orders"
 };
