4c6415255bdc5f06a2cc779abe6f01fd3382ef20
Philipp Awwad <pa@awwad.eu>
2026-09-07 06:56:51 +0200

    Vorschau 2026-09-07 06:56

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://commander-peripheral-margaret-fairfield.trycloudflare.com/api/orders"
+  orderEndpoint: "https://infants-johns-collection-representing.trycloudflare.com/api/orders"
 };
