09f9438cdbb92a8c9ab8c2e79b2111b4a3332942
Philipp Awwad <pa@awwad.eu>
2026-09-07 13:04:23 +0200

    Vorschau 2026-09-07 13:04

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://removal-ends-mandatory-absorption.trycloudflare.com/api/orders"
+  orderEndpoint: "https://maintenance-bluetooth-tracks-marathon.trycloudflare.com/api/orders"
 };
