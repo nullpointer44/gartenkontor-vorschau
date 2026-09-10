af710b92dded8861521dad0f117e87ca56939221
Philipp Awwad <pa@awwad.eu>
2026-09-10 09:59:45 +0200

    Vorschau 2026-09-10 09:59

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://truck-newton-nation-suspended.trycloudflare.com/api/orders"
+  orderEndpoint: "https://attribute-spoke-bodies-submit.trycloudflare.com/api/orders"
 };
