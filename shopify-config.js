a7eb04151221af214de799a095b6331a2628007a
Philipp Awwad <pa@awwad.eu>
2026-09-12 13:53:21 +0200

    Vorschau 2026-09-12 13:53

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://clips-wanted-merge-eco.trycloudflare.com/api/orders"
+  orderEndpoint: "https://indicator-optimum-compliant-connectors.trycloudflare.com/api/orders"
 };
