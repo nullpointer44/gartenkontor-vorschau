30bd88894ea2eb2d9e72e6acfdc44700107dc122
Philipp Awwad <pa@awwad.eu>
2026-09-14 18:04:59 +0200

    Vorschau 2026-09-14 18:04

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://certification-files-brother-informal.trycloudflare.com/api/orders"
+  orderEndpoint: "https://affair-erik-controls-obtain.trycloudflare.com/api/orders"
 };
