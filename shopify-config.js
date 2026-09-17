b401aa02ca3b14f7697210a8e1530a9ade33662e
Philipp Awwad <pa@awwad.eu>
2026-09-17 03:14:46 +0200

    Vorschau 2026-09-17 03:14

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://length-attractions-recorders-gba.trycloudflare.com/api/orders"
+  orderEndpoint: "https://examine-mechanisms-mine-pounds.trycloudflare.com/api/orders"
 };
