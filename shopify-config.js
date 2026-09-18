a310b5e676b637673ebf3dcc06b92efddc8d123f
Philipp Awwad <pa@awwad.eu>
2026-09-18 23:05:57 +0200

    Vorschau 2026-09-18 23:05

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://dangerous-rings-settings-demands.trycloudflare.com/api/orders"
+  orderEndpoint: "https://sys-speaking-collection-austin.trycloudflare.com/api/orders"
 };
