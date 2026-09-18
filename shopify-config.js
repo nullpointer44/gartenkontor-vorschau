161c35eee4aef151bbf27078d3feaab6fb7c4eef
Philipp Awwad <pa@awwad.eu>
2026-09-18 10:28:15 +0200

    Vorschau 2026-09-18 10:28

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://pdf-heritage-mba-lemon.trycloudflare.com/api/orders"
+  orderEndpoint: "https://replacing-decades-dns-liquid.trycloudflare.com/api/orders"
 };
