50d4ee4331b1e65eadc45282ed84b71cf2af61cb
Philipp Awwad <pa@awwad.eu>
2026-09-19 03:30:23 +0200

    Vorschau 2026-09-19 03:30

--- shopify-config.js
+++ shopify-config.js
@@ -6,5 +6,5 @@ window.SHOPIFY_CONFIG = {
   storefrontToken: "",   // Storefront API access token (public)
   apiVersion: "2025-07",
   // Bestell-Router des Admin-Tools (Bestellungen der Vorschau-Kasse werden an die Außendienstler gemeldet; leer = aus)
-  orderEndpoint: "https://moment-fruits-diffs-cluster.trycloudflare.com/api/orders"
+  orderEndpoint: "https://cow-battle-loading-fri.trycloudflare.com/api/orders"
 };
